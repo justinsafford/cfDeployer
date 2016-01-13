88b815ed476ffcf0b6dbe482037705a51c2afbdf
Justin Safford <justin.safford@gmail.com>
2016-01-13 07:15:25 -0600

    initial commit

---
+++ src/main/java/com/justinsafford/cfUtilities/CfDeployerApplication.java
@@ -0,0 +1,12 @@
+package com.justinsafford.cfUtilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDeployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfDeployerApplication.class, args);
+	}
+}
