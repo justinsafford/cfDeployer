9a06d21390a95144990e747771200291ba27f333
Justin Safford <justin.safford@gmail.com>
2016-01-23 11:40:03 -0600

    GET controller added to return all apps within space

--- src/main/java/com/justinsafford/cfUtilities/app/AppController.java
+++ src/main/java/com/justinsafford/cfUtilities/app/AppController.java
@@ -4,6 +4,7 @@ import com.justinsafford.cfUtilities.cloudClient.CloudClientEntity;
 import com.justinsafford.cfUtilities.cloudClient.CloudClientRepository;
 import org.cloudfoundry.client.lib.CloudCredentials;
 import org.cloudfoundry.client.lib.CloudFoundryClient;
+import org.cloudfoundry.client.lib.domain.CloudApplication;
 import org.cloudfoundry.client.lib.domain.Staging;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AppController {
@@ -54,5 +56,39 @@ public class AppController {
         services.add(applicationRequest.getService());
 
         cloudFoundryClient.createApplication(applicationRequest.getApplicationName(), new Staging(), 1024, uris, services);
+    }
+
+    @RequestMapping(
+            value = "/application/{cloudClientId}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<String> getAllApps(@PathVariable String cloudClientId) {
+
+        CloudClientEntity cloudClientEntity = cloudClientRepository.findOne(cloudClientId);
+
+        CloudCredentials cloudCredentials = new CloudCredentials(cloudClientEntity.getCloudUser(), cloudClientEntity.getCloudPass());
+        URL url = null;
+        try {
+            url = new URL("HTTP", "api.run.pivotal.io", 80, "");
+        } catch (MalformedURLException e) {
+            System.out.println("something bad happened here");
+        }
+        String cloudOrg = cloudClientEntity.getCloudOrg();
+        String cloudSpace = cloudClientEntity.getCloudSpace();
+        CloudFoundryClient cloudFoundryClient = new CloudFoundryClient(
+                cloudCredentials,
+                url,
+                cloudOrg,
+                cloudSpace
+        );
+
+        List<CloudApplication> cloudAppsFound = cloudFoundryClient.getApplications();
+        List<String> cloudAppsList = new ArrayList<>();
+
+        cloudAppsFound
+                .forEach(cloudApplication -> cloudAppsList.add(cloudApplication.getName()));
+
+        return cloudAppsList;
     }
 }
